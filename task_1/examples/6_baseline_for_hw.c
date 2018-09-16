886aeb59e4f76f9a79c952da8d3adc9ec637c856
bbg1ve <mccgeorge@mail.ru>
2018-09-16 16:13:58 +0300

    execvp error added

--- task_1/examples/6_baseline_for_hw.c
+++ task_1/examples/6_baseline_for_hw.c
@@ -1,31 +1,65 @@
 /* this example shows how to implement the simplest solution of homework 1. */
 #include <unistd.h>
 #include <stdio.h>
-/* put your headers here */
+#include <stdlib.h>
+#include <stdio.h>
+#include <iso646.h>
+#include <sys/wait.h>
+#include <ctype.h>
+#include <sys-types.h>
+#define N 1000
 
-static void run_cmd(const char *cmd)
-{
-  const pid_t = fork();
-  if (pid < 0) {
-    printf("fork failed!\n");
-  }
-  if (pid) {
-    waitpid(pid, NULL,0);
-    /* add status check here (use examples) */
-    return;
-  }
-  
-  char **args=parse_cmd(cmd); // implement this function by yourself
-  execvp(args[0], args);
-  printf("exec* failed\n");
+int main() {
+        char del[] = " ,\n";
+        char *rest1 = "exit";
+        char *rest2 = "close";
+        printf("command: 'close' or 'exit' to leave program\n");
+        while (1!=0){
+                char *p;
+                char *s = (char *)malloc(N*sizeof(char));// = (char *)malloc(N*sizeof(char));
+                int i = 0;
+                int j = 0;
+
+                scanf("%[^\n]%*c", s);
+                if ((strcmp(s, rest1) == 0) or (strcmp(s, rest2) == 0)){
+                        free(s);
+                        break;
+                }
+                const pid_t pid = fork();
+                if (pid < 0)
+                        printf("fatal error\n");
+                if (pid){
+                        int status;
+                        waitpid(pid, &status, 0);
+                        printf("Return code: %d\n", WEXITSTATUS(status));
+                        printf("Return pid: %d\n", getpid());
+                }
+                else{
+                        char **help1 = (char **)malloc(N*sizeof(char*));
+                        int k=0;
+                        for (;k<N;k++)
+                                help1[k] = NULL;
+
+//              for (i = 0; i < 10; i++)
+//                      help1[i] = 0;
+                        printf("String: %s", s);
+                        for( p = strtok(s, del); p != NULL; p = strtok(NULL, del)) {
+                                help1[j] = p;
+                                j += 1;
+                        }
+
+//              char **help2 = realloc(help1, j);
+                        int q;
+                        q = execvp(help1[0], help1);
+                        if (q == -1) {
+                                printf("fatal error");
+                                exit(42);
+                        }
+                        free(s);
+                        free(help1);
+                }
+
+        }
+        return 0;
 }
 
-int main()
-{
-  while(1) {
-    char *cmd;
-    // read cmd here
-    run_cmd(cmd);
-  }
-  return 0;
- }
